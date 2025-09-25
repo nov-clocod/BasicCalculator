929dc8edce7952af06abde6d8e0e99a75a3f780b
Sthealth <AW@example.com>
2025-09-25 11:20:24 -0400

    Completed Mod3 - Exercise 3 - Basic Calculator

--- src/main/java/com/pluralsight/Calculator.java
+++ src/main/java/com/pluralsight/Calculator.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class Calculator {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("What math operation would you like to do? This is fake! It's all multiplication :)");
+        String userOperation = myScanner.nextLine();
+
+        System.out.println("Enter your first number:");
+        float firstNumber = myScanner.nextFloat();
+
+        System.out.println("Enter your second number:");
+        float secondNumber = myScanner.nextFloat();
+
+        float multiplicationResult = firstNumber * secondNumber;
+
+        System.out.println("Your result of " + firstNumber + " and " + secondNumber + " is: " + multiplicationResult);
+    }
 }
